d73a07ca9e6eb48c11ef459d05bf7b501e2ff7b0
krzysztofee <gitaragra@gmail.com>
2023-06-18 18:27:40 +0200

    Add montht slists

--- godziny/src/components/sidebar/IndexSidebar.tsx
+++ godziny/src/components/sidebar/IndexSidebar.tsx
@@ -13,59 +13,6 @@ import MonthsList from "./MonthsList";
 
 const IndexSidebar = () => {
   const navigate = useNavigate();
-  const { monthURL } = useParams();
-  const { data } = useColumnsQuery(undefined);
-  const { databaseMonthsCollection } = useDataBaseValues(monthURL);
-
-  const databaseMonthsDatesSorted =
-    data && databaseMonthsCollection
-      ? databaseMonthsCollection
-          .map(month => month.month)
-          .sort((date1: any, date2: any) => {
-            if (date1 < date2) {
-              return -1;
-            } else if (date1 > date2) {
-              return 1;
-            } else {
-              return 0;
-            }
-          })
-      : [];
-
-  const databaseMonthsDatesToString =
-    data &&
-    databaseMonthsDatesSorted?.map((monthDate: any) => {
-      const monthToDateFormat =
-        data && databaseMonthsDatesSorted && new Date(monthDate);
-
-      return new Intl.DateTimeFormat("pl-PL", {
-        year: "numeric",
-        month: "long",
-        timeZone: "UTC",
-      }).format(monthToDateFormat);
-    });
-
-  // const date = new Date();
-  // const currYearNum = new Intl.DateTimeFormat("pl", {
-  //   year: "numeric",
-  // }).format(date);
-
-  // const currMonthNum = new Intl.DateTimeFormat("pl", {
-  //   month: "2-digit",
-  // }).format(date);
-
-  const url = databaseMonthsDatesSorted[databaseMonthsDatesSorted.length - 1];
-
-  useEffect(() => {
-    const date = new Date();
-    const currYearNum = new Intl.DateTimeFormat("pl", {
-      year: "numeric",
-    }).format(date);
-    const currMonthNum = new Intl.DateTimeFormat("pl", {
-      month: "2-digit",
-    }).format(date);
-    data && url && navigate(`/miesiac/${currYearNum}-${currMonthNum}`);
-  }, []);
 
   const urlPrintNavBar = useLocation().pathname;
 
@@ -74,6 +21,10 @@ const IndexSidebar = () => {
   const handleClose = () => setShow(false);
   const toggleShow = () => setShow(s => !s);
 
+  const logout = () => {
+    auth.signOut();
+    navigate("/");
+  };
 
   return (
     <>
@@ -104,24 +55,6 @@ const IndexSidebar = () => {
             </Offcanvas.Header>
             <Offcanvas.Body>
               <MonthForm />
-
-              {/* {data === undefined || data === null ? (
-                <p>Brak danych</p>
-              ) : (
-                databaseMonthsDatesToString.map((month: any, idx: any) => {
-                  return (
-                    <div key={month}>
-                      <Link
-                        to={`/miesiac/${databaseMonthsDatesSorted[idx]}`}
-                        key={month}
-                      >
-                        {month}
-                      </Link>
-                    </div>
-                  );
-                })
-              )} */}
-
               <MonthsList />
               <DayForm />
             </Offcanvas.Body>
--- godziny/src/components/sidebar/MonthsList.tsx
+++ godziny/src/components/sidebar/MonthsList.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useColumnsQuery } from "../../services/apiSlice";
+import useDataBaseValues from "../../pages/monthPanel/useDataBaseValues";
+
 const MonthsList = () => {
+  const { data } = useColumnsQuery(undefined);
+  const { monthURL } = useParams();
+  const { databaseMonthsCollection } = useDataBaseValues(monthURL);
+
+  const databaseMonthsDatesSorted =
+    data && databaseMonthsCollection
+      ? databaseMonthsCollection
+          .map(month => month.month)
+          .sort((date1: any, date2: any) => {
+            if (date1 < date2) {
+              return -1;
+            } else if (date1 > date2) {
+              return 1;
+            } else {
+              return 0;
+            }
+          })
+      : [];
+
+  const databaseMonthsDatesToString =
+    data &&
+    databaseMonthsDatesSorted?.map((monthDate: any) => {
+      const monthToDateFormat =
+        data && databaseMonthsDatesSorted && new Date(monthDate);
+
+      return new Intl.DateTimeFormat("pl-PL", {
+        year: "numeric",
+        month: "long",
+        timeZone: "UTC",
+      }).format(monthToDateFormat);
+    });
+
   return (
     <>
       {data === undefined || data === null ? (
@@ -9,25 +45,20 @@ const MonthsList = () => {
       ) : (
         databaseMonthsDatesToString.map((month: any, idx: any) => {
           return (
-            <div key={month}>
-              <Link
-                to={`/miesiac/${databaseMonthsDatesSorted[idx]}`}
-                key={month}
-              >
-                {month}
-              </Link>
-            </div>
+            <ListGroup key={month}>
+              <ListGroup.Item className="border-0 p-0 px-1">
+                <Link
+                  to={`/miesiac/${databaseMonthsDatesSorted[idx]}`}
+                  key={month}
+                  style={{ textDecoration: "none" }}
+                >
+                  {month}
+                </Link>
+              </ListGroup.Item>
+            </ListGroup>
           );
         })
       )}
-
-      <ListGroup>
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
     </>
   );
 };
