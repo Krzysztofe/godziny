e5c86120c2ec259c7356851284dda0e950a30a6f
krzysztofee <gitaragra@gmail.com>
2023-08-13 23:17:56 +0200

    Add basename for ghPages

--- godziny/src/App.tsx
+++ godziny/src/App.tsx
@@ -18,7 +18,7 @@ const IndexSettings = lazy(
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/godziny">
       <Suspense fallback={<LoadingPage />}>
         <IndexSidebar />
         <Routes>
