58835a80eef47672837f664bf55c456720c6a56c
krzysztofee <gitaragra@gmail.com>
2023-12-01 01:11:27 +0100

    Add month name to SidebarMenuButton

--- godziny/src/components/sidebar/SidebarMenuButton.tsx
+++ godziny/src/components/sidebar/SidebarMenuButton.tsx
@@ -1,27 +1,29 @@
 import Button from "react-bootstrap/Button";
 import useWindowWidth from "../../hooks/useWindowWidth";
+import useMonthURLToString from "../../hooks/useMonthURLToString";
 
 type Props = {
-  toggleShow: ()=>void;
+  toggleShow: () => void;
 };
 
 const SidebarMenuButton = (props: Props) => {
   const { windowWidth } = useWindowWidth();
+  const { monthURLStringFormat } = useMonthURLToString();
 
   return (
     <Button
       variant="info"
       onClick={props.toggleShow}
-      className={`rounded-0 p-0 fw-medium w-100 ${
-        windowWidth >= 500 ? "d-none" : "d-block"
-      }`}
+      className={`${
+        windowWidth >= 500 ? "d-none" : "d-flex"
+      } justify-content-center align-items-center rounded-0 p-0 fw-medium w-100 px-4`}
       style={{
         position: "fixed",
         height: "2.5rem",
         bottom: 0,
       }}
     >
-      Menu
+      <div>Menu:&nbsp; </div> <div> {monthURLStringFormat}</div>
     </Button>
   );
 };
