1154c84f1c19833bebff9c36a9ba133ebc55b744
krzysztofe <gitaragra@gmai.com>
2025-11-18 22:20:49 +0100

    Add vercel url

--- godziny/src/data/URL.tsx
+++ godziny/src/data/URL.tsx
@@ -1,7 +1,7 @@
-export const URL_LOGIN_DATA = "http://localhost:8000/auth";
-export const URL_USERS_DATA = "http://localhost:8000/users";
-export const URL_MONTHS_DATA = "http://localhost:8000/months";
+// export const URL_LOGIN_DATA = "http://localhost:8000/auth";
+// export const URL_USERS_DATA = "http://localhost:8000/users";
+// export const URL_MONTHS_DATA = "http://localhost:8000/months";
 
-// export const URL_LOGIN_DATA = "https://godziny-server.vercel.app/auth";
-// export const URL_USERS_DATA = "https://godziny-server.vercel.app/users";
-// export const URL_MONTHS_DATA = "https://godziny-server.vercel.app/months";
+export const URL_LOGIN_DATA = "https://godziny-server.vercel.app/auth";
+export const URL_USERS_DATA = "https://godziny-server.vercel.app/users";
+export const URL_MONTHS_DATA = "https://godziny-server.vercel.app/months";
